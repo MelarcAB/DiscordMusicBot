6cddd2006790969e88f1b73fbf05d7b469ebfb3a
Melarc <melarc.ab@gmail.com>
2023-09-17 13:12:32 +0200

    funciona con enlaces que envio por parametro

--- newBot.js
+++ newBot.js
@@ -3,65 +3,130 @@ const { Poru } = require("poru");
 require('dotenv').config();
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.MessageContent,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 const nodes = [
-  {
-    name: "local-node",
-    host: "194.163.135.84",
-    port: 2333,
-    password: "youshallnotpass",
-  },
+    {
+        name: "local-node",
+        host: "194.163.135.84",
+        port: 2333,
+        password: "youshallnotpass",
+    },
 ];
 
 const PoruOptions = {
-  library: "discord.js",
+    library: "discord.js",
 };
 
 client.poru = new Poru(client, nodes, PoruOptions);
 
 client.poru.on("trackStart", (player, track) => {
-  const channel = client.channels.cache.get(player.textChannel);
-  return channel.send(`Now playing \`${track.info.title}\``);
+    const channel = client.channels.cache.get(player.textChannel);
+    return channel.send(`Now playing \`${track.info.title}\``);
 });
 
 client.once("ready", () => {
-  console.log("Ready!");
-  client.poru.init(client);
+    console.log("Ready!");
+    client.poru.init(client);
 });
 
 client.on("messageCreate", async (message) => {
-  if (message.content.toLowerCase() === "!hola") {
-    if (!message.member.voice.channel) {
-      return message.reply("¡Debes estar en un canal de voz para usar este comando!");
+    const msg = message.content.toLowerCase();
+    const msg_raw = message.content;
+    if (!msg.startsWith("!")) return;
+    console.log(msg);
+    //obtener la primera palabra del mensaje y validar si empieza con !
+    const command = msg.split(" ")[0].substring(1);
+    let trackURL = "https://www.youtube.com/watch?v=dtD_xGvkWLk";
+
+    switch (command) {
+        case "play":
+
+            //validar que hay un segundo parametro
+            if (msg.split(" ").length < 2) {
+                message.reply("¡Debes proporcionar un link!");
+                return;
+                break;
+            }
+
+            //validar que el usuario este en un canal de voz
+            if (!message.member.voice.channel) {
+                message.reply("¡Debes estar en un canal de voz para usar este comando!");
+                return;
+                break;
+            }
+
+            //obtener el link de la cancion
+            trackURL = msg_raw.split(" ")[1];
+            console.log(trackURL);
+            //validar que el link sea de youtube
+            if (!trackURL.includes("youtube")) {
+                message.reply("¡Debes proporcionar un link de youtube!");
+                return;
+                break;
+            }
+
+            //validar que el link sea valido
+            const res = await client.poru.resolve(trackURL);
+            if (res.loadType === "LOAD_FAILED" || res.loadType === "NO_MATCHES") {
+                return message.reply("Hubo un error al cargar la canción.");
+            }
+
+            //crear la conexion
+            const player = client.poru.createConnection({
+                guildId: message.guild.id,
+                voiceChannel: message.member.voice.channelId,
+                textChannel: message.channel.id,
+                deaf: true,
+            });
+
+            //agregar la cancion a la cola
+            const track = res.tracks[0];
+            track.info.requester = message.author;
+            player.queue.add(track);
+
+            //reproducir la cancion
+            if (!player.isPlaying && player.isConnected) player.play();
+            break;
+
+
+
+        default:
+            //no responder, ya que no es un comando
+            //message.reply("Comando no encontrado");
+            break;
     }
-
-    const trackURL = "https://www.youtube.com/watch?v=dtD_xGvkWLk";
-    const res = await client.poru.resolve(trackURL);
-
-    if (res.loadType === "LOAD_FAILED" || res.loadType === "NO_MATCHES") {
-      return message.reply("Hubo un error al cargar la canción.");
-    }
-
-    const player = client.poru.createConnection({
-      guildId: message.guild.id,
-      voiceChannel: message.member.voice.channelId,
-      textChannel: message.channel.id,
-      deaf: true,
-    });
-
-    const track = res.tracks[0];
-    track.info.requester = message.author;
-    player.queue.add(track);
-
-    if (!player.isPlaying && player.isConnected) player.play();
-  }
+    /*
+        if (message.content.toLowerCase() === "!hola") {
+            if (!message.member.voice.channel) {
+                return message.reply("¡Debes estar en un canal de voz para usar este comando!");
+            }
+    
+            const res = await client.poru.resolve(trackURL);
+    
+            if (res.loadType === "LOAD_FAILED" || res.loadType === "NO_MATCHES") {
+                return message.reply("Hubo un error al cargar la canción.");
+            }
+    
+            const player = client.poru.createConnection({
+                guildId: message.guild.id,
+                voiceChannel: message.member.voice.channelId,
+                textChannel: message.channel.id,
+                deaf: true,
+            });
+    
+            const track = res.tracks[0];
+            track.info.requester = message.author;
+            player.queue.add(track);
+    
+            if (!player.isPlaying && player.isConnected) player.play();
+        }*/
 });
 
 client.login(process.env.TOKEN); 
